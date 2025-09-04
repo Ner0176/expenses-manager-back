382bee2cf046fb2648befb1a19fb2690fcef3e18
Esteve <esteve.17mas@gmail.com>
2025-09-04 19:47:55 +0200

    feat: config typeorm + sqlite + allow cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
   providers: [AppService],
+  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      synchronize: true,
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? process.env.FRONT_LOCAL_URL
+        : process.env.FRONT_PROD_URL,
+  });
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
